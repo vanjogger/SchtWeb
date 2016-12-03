59b3e0643acfc4c920b2c627bef04b3f3365f869
vanjoger <rpg0209>
2016-12-03 15:16:07 +0800

    接口

--- src/main/java/com/scht/front/bean/RetResult.java
+++ src/main/java/com/scht/front/bean/RetResult.java
@@ -75,16 +75,7 @@ public class RetResult implements Serializable {
         public static final String Sms_Fail = "10004";
         //短信验证码错误
         public static final String ValidateCode_Error = "10005";
-        
-		// 您关注的用户在您黑名单中，请取消黑名单后再进行操作
-		public static final String User_In_My_Blacklist = "10020";
-		
-    	// 您别对方拉入黑名单中，无法进行关注操作
-		public static final String I_In_Concern_Blacklist = "10021";
-		
-		//此用户，您已举报过，不要重复举报
-		public static final String  User_Repot_Exist= "10022";
-		
+
         //手机号码已注册
         public static final String User_Already_Reg = "10011";
         //短信发送频繁
@@ -93,29 +84,7 @@ public class RetResult implements Serializable {
         public static final String Sms_InValid = "10013";
         //用户旧密码错误
         public static final String User_Old_Pwd_Error = "10014";
-        //活动不存在
-        public static final String Activity_Not_Exist = "20001";
-        //重复的加入
-        public static final String Activity_SignIn_Repeat = "20002";
-        // 重复的退出
-        public static final String Activity_SignOut_Repeat = "20003";
-        //当前用户与活动发起人不一致
-        public static final String Activity_User_NotMatch = "20004";
-        //参与者不能为空
-        public static final String Member_NotEmpty = "20005";
-        
-        //景点不存在
-        public static final String Spot_Not_Exist = "30001";
-        //已收藏
-        public static final String ALREADY_FAVORITE = "30002";
-        //收藏信息不存在
-        public static final String FAVORITE_NOT_Exist = "30003";
-        
-        //游记不存在
-        public static final String Travel_Not_Exist = "50001";
-        
-      //游记行程不存在
-        public static final String Travel_Trip_Not_Exist = "50002";
+
 		
 	}
 }
